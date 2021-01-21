c819f0b7b35c8757da0c9c39391f7692155a848b
Tomone Akashi <t.akashi@uzuz.jp>
2021-01-21 22:34:51 +0900

    Change

--- public/js/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('js/service-worker.js')
-      .then(registration => console.log('registered', registration))
-  }
-});
